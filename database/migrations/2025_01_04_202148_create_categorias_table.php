08a0bd5bfa0e7acf26de49352b0dbaf1eb98159a
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-05 16:15:06 -0300

    factory

--- database/migrations/2025_01_04_202148_create_categorias_table.php
+++ database/migrations/2025_01_04_202148_create_categorias_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->text('descricao');
             $table->timestamps();
         });
     }
