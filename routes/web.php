47a133ff81620d42148899c53df06d4c5beb8884
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-05 14:43:40 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\ProdutoController;
+use Illuminate\Support\Facades\Route;
+
+Route::resource('produtos', ProdutoController::class);
+
+Route::get('/', [ProdutoController::class, 'index']);
