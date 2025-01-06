b6e89fa8a819b28be2901ca67ad5567188cbf2b2
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-06 12:30:38 -0300

    include e component

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -6,5 +6,15 @@
 @section('conteudo')
 
     <h1>Essa é nossa home</h1>
+
+    @include('includes.mensagem', ['titulo' => 'mensagem de sucesso!'])
+
+    @component('components.sidebar')
+
+        @slot('paragrafo')
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero architecto odit fugit assumenda ea! Ipsam maxime laborum ullam praesentium dignissimos cupiditate dolorum sed rerum suscipit, quaerat nemo! Velit, dolorem saepe?
+        @endslot
+        
+    @endcomponent
     
 @endsection
