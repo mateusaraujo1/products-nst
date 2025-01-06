4b86634c6c71f9d862a70ccaa723ee6a87cf2127
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-06 20:46:30 -0300

    stacks e push

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -18,3 +18,13 @@
     @endcomponent
     
 @endsection
+
+@push('style')
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+@endpush
+
+@push('script')
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+@endpush
