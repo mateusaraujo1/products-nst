f996495d4ef125fc84ba87203c63264e2656e26c
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-08 09:39:15 -0300

    add dropdown categoria

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -11,14 +11,27 @@
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
+    
+
 </head>
 <body>
+
+  <ul id='dropdown1' class='dropdown-content'>
+    
+    @foreach ($categoriasMenu as $categoria)
+
+      <li><a href="#!">{{$categoria->nome}}</a></li>
+        
+    @endforeach
+    
+  </ul>
 
     <nav class="red">
         <div class="nav-wrapper container">
           <a href="#" class="brand-logo center">Curso Laravel</a>
           <ul id="nav-mobile" class="left">
             <li><a href="">Home</a></li>
+            <li><a href="" class="dropdown-trigger" data-target="dropdown1">Categorias <i class="material-icons right">expand_more</i> </a></li>
             <li><a href="">Carrinho</a></li>
           </ul>
         </div>
@@ -28,5 +41,16 @@
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script>
+      var elemDrop = document.querySelectorAll('.dropdown-trigger');
+
+      var instanceDrop = M.Dropdown.init(elemDrop, {
+          coverTrigger:false,
+          constrainWidth:false 
+      });
+    </script>
+
+    
 </body>
 </html>
