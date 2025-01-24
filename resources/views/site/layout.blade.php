21814922e0679826c2888d8a3553e7264d32f5ff
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-24 16:52:10 -0300

    exibindo dados de user logado

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -16,24 +16,39 @@
 </head>
 <body>
 
+  
   <ul id='dropdown1' class='dropdown-content'>
     
     @foreach ($categoriasMenu as $categoria)
-
-      <li><a href="{{ route('site.categoria', $categoria->id) }}">{{$categoria->nome}}</a></li>
-        
+    
+    <li><a href="{{ route('site.categoria', $categoria->id) }}">{{$categoria->nome}}</a></li>
+    
     @endforeach
     
   </ul>
 
+  <ul id='dropdown2' class='dropdown-content'>
+
+      <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+        
+  </ul>
+
     <nav class="red">
         <div class="nav-wrapper container">
+
           <a href="#" class="brand-logo center">Curso Laravel</a>
+
           <ul id="nav-mobile" class="left">
             <li><a href="{{ route('site.index') }}">Home</a></li>
             <li><a href="" class="dropdown-trigger" data-target="dropdown1">Categorias <i class="material-icons right">expand_more</i> </a></li>
             <li><a href="{{ route('site.carrinho') }}">Carrinho <span class="new badge blue" data-badge-caption="">{{ \Cart::getContent()->count() }}</span> </a></li>
           </ul>
+
+          <ul id="nav-mobile" class="right">
+            <li><a href="" class="dropdown-trigger" data-target="dropdown2">Olá {{ auth()->user()->first_name }} <i class="material-icons right">expand_more</i> </a></li>
+          </ul>
+
+
         </div>
     </nav>
     
